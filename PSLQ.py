96464b08a6a84883dcc1fab2825c14117f4282d9
Colin McRae <colinm2718@gmail.com>
2023-02-08 23:44:40 -0500

    Fixed indexOfWeightedMax

--- PSLQ.py
+++ PSLQ.py
@@ -7,48 +7,62 @@ from numpy import array, matmul, transpose
 from numpy.linalg import det, inv
 from math import sqrt
 
-# Inputs
-errThresh = 0.00001
-gamma = sqrt(4/3)
-unnormalizedX = [2, 3, -5]
-iteratioinAfterWhichToStop = 3
-iterationAfterWhichToDisplay = [2]
-
-def testEqual1D(numpyMatrix, plainList, leftSide, rightSide):
+def testEqual1D(numpyMatrix_in, plainList_in, leftSide_in, rightSide_in, errThresh_in):
     maxErr = 0
-    for i in range(len(plainList)):
-        err = abs(numpyMatrix[i] - plainList[i])
+    for i in range(len(plainList_in)):
+        err = abs(numpyMatrix_in[i] - plainList_in[i])
         if err > maxErr:
             maxErr = err
-    if maxErr < errThresh:
-        print("=====", leftSide, "=", rightSide)
+    if maxErr < errThresh_in:
+        print("=====", leftSide_in, "=", rightSide_in)
     else:
-        print("=====", leftSide, "=", numpyMatrix, "!=", plainList, "=", rightSide)
+        print("=====", leftSide_in, "=", numpyMatrix_in, "!=", plainList_in, "=", rightSide_in)
         print("       Hit <Enter> to continue.")
         input()
 
-def testEqual2D(numpyMatrix, plainList, leftSide, rightSide):
+def testEqual2D(numpyMatrix_in, plainList_in, leftSide_in, rightSide_in, errThresh_in):
     maxErr = 0
-    for i in range(len(plainList)):
-        for j in range(len(plainList[i])):
-            err = abs(numpyMatrix[i, j] - plainList[i][j])
+    for i in range(len(plainList_in)):
+        for j in range(len(plainList_in[i])):
+            err = abs(numpyMatrix_in[i, j] - plainList_in[i][j])
             if err > maxErr:
                 maxErr = err
-    if maxErr < errThresh:
-        print("=====", leftSide, "=", rightSide)
+    if maxErr < errThresh_in:
+        print("=====", leftSide_in, "=", rightSide_in)
     else:
-        print("=====", leftSide, "=", numpyMatrix, "!=", plainList, "=", rightSide)
+        print("=====", leftSide_in, "=", numpyMatrix_in, "!=", plainList_in, "=", rightSide_in)
         print("       Hit <Enter> to continue.")
         input()
 
+def printFloatMatrix(matrix_in, name_in):
+    print("==========", name_in + ":")
+    numRows = len(matrix_in)
+    for i in range(numRows):
+        numColumns = len(matrix_in[i])
+        print("  ", end='')
+        for j in range(numColumns):
+            print(matrix_in[i][j], " ", end='')
+            # print('{:10.2} '.format(matrix_in[i][j]), end='')
+        print()
 
-def nint(a):
-    if a > 0:
-        return int(0.5 + a)
-    return -int(0.5 - a)
+def printIntMatrix(matrix_in, name_in):
+    print("==========", name_in + ":")
+    numRows = len(matrix_in)
+    for i in range(numRows):
+        numColumns = len(matrix_in[i])
+        print("  ", end='')
+        for j in range(numColumns):
+            print(matrix_in[i][j], " ", end='')
+            # print('{:-10} '.format(matrix_in[i][j]), end='')
+        print()
 
-def getIdentity(dim):
-    return [[1 if i == j else 0 for j in range(dim)] for i in range(dim)]
+def nint(a_in):
+    if a_in > 0:
+        return int(0.5 + a_in)
+    return -int(0.5 - a_in)
+
+def getIdentity(dim_in):
+    return [[1 if i == j else 0 for j in range(dim_in)] for i in range(dim_in)]
 
 def getNormalizedX(x_in, n_in):
     # Page 4: "Assume that x is a unit vector"
@@ -56,12 +70,12 @@ def getNormalizedX(x_in, n_in):
     normX = sqrt(sum([x_in[i] * x_in[i] for i in range(n_in)]))
     return [ x_in[i] / normX for i in range(n_in) ]
 
-def indexOfWeightedMax(H_in, gamma_in):
+def indexOfWeightedMax(H_in, gamma_in, n_in):
     rval          = 0
     maxOnDiagonal = H_in[0][0]
     powerOfGamma  = gamma_in
-    for i in range(1, n-1):
-        testValue = powerOfGamma * H_in[i][i]
+    for i in range(1, n_in - 1):
+        testValue = powerOfGamma *  abs(H_in[i][i])
         if testValue > maxOnDiagonal:
             maxOnDiagonal = testValue
             rval = i
@@ -83,13 +97,13 @@ def getH(x_in, s_in, n_in):
     # On the diagonal:       s[i+1]/s[i]          (rows 0, ... n - 2)
     # Right of the diagonal: 0                    (rows 0, ... n - 3)
     H = []
-    for i in range(n):
+    for i in range(n_in):
         row = []       # Overwritten by entries to the left of the diagonal if i > 0
         if 1 <= i:     # Write entries to the left of the diagonal
             row = [-x_in[i] * x_in[j] / (s_in[j] * s_in[j+1]) for j in range(i)]
-        if i <= n - 2: # Write an entry on the diagonal
+        if i <= n_in - 2: # Write an entry on the diagonal
             row.append(s_in[i + 1] / s_in[i])
-        if i <= n - 3: # Write 0s to the right of the diagonal
+        if i <= n_in - 3: # Write 0s to the right of the diagonal
             for j in range(i + 1, n_in - 1):
                 row.append(0)
         H.append(row)
@@ -168,7 +182,7 @@ def getD(H_in, n_in):
     D = [[nint(D0[i][j]) for j in range(n_in)] for i in range(n_in)]
     return D, D0
 
-def getE(D_in):
+def getE(D_in, n_in):
     # Page 4: "The inverse of D, namely E [= D^−1}, can be defined with
     #         recursion similar to the above [but we don't need the efficiency
     #         of the recursions given in the paper, so just use numpy]"
@@ -177,81 +191,96 @@ def getE(D_in):
     # error.
     #
     E_nxn = inv(array(D_in))
-    E = [[nint(E_nxn[i, j]) for j in range(n)] for i in range(n)]
+    E = [[nint(E_nxn[i, j]) for j in range(n_in)] for i in range(n_in)]
     return E
 
-# Initializations
-n = len(unnormalizedX)
-x = getNormalizedX(unnormalizedX, n)
-s = getS(x, n)
-H = getH(x, s, n)
+def run(unnormalizedX_in, errThresh_in, gamma_in, iteratioinAfterWhichToStop_in):
 
-A = getIdentity(n)
-B = getIdentity(n)
+    # Initializations
+    n = len(unnormalizedX_in)
+    x = getNormalizedX(unnormalizedX_in, n)
+    s = getS(x, n)
+    H = getH(x, s, n)
 
-# For use in testing invariants
-x_1xn = array(x)
-H0_nxnm1 = array(H)
-testEqual2D(matmul(transpose(H0_nxnm1), H0_nxnm1), getIdentity(n - 1), "HtH", "I")
-H0cumG_nxnm1 = array(H)
+    A = getIdentity(n)
+    B = getIdentity(n)
 
-# PSLQ Iterations
-numIterations = 0
-while numIterations < iteratioinAfterWhichToStop:
-    # Page 5: "Replace H by DH."
-    #
-    D, D0 = getD(H, n)
-    HDiagonal = [[H[i][j] if i == j else 0 for j in range(n - 1)] for i in range(n)]
-    testEqual2D(matmul(array(D0), array(H)), HDiagonal, "D0 H", "diagonal of H")
-    DH_nxnm1 = matmul(array(D), array(H))
-    H = [[DH_nxnm1[i, j] for j in range(n - 1)] for i in range(n)]
+    # For use in testing invariants
+    x_1xn = array(x)
+    H0_nxnm1 = array(H)
+    testEqual2D(matmul(transpose(H0_nxnm1), H0_nxnm1), getIdentity(n - 1), "HtH", "I", errThresh_in)
+    H0cumG_nxnm1 = array(H)
 
-    # Page 5: "Select an integer j, [0 <= j < n − 1 in our zero-based indexing]
-    #         such that gamma^j |H[j][j]| >= gamma^i |H[i][i]| for all i,
-    #         [0 <= i < n − 1 in our indexing]
-    j = indexOfWeightedMax(H, gamma)
+    # PSLQ Iterations
+    numIterations = 0
+    while numIterations < iteratioinAfterWhichToStop_in:
+        # Page 5: "Replace H by DH."
+        #
+        D, D0 = getD(H, n)
+        HDiagonal = [[H[i][j] if i == j else 0 for j in range(n - 1)] for i in range(n)]
+        testEqual2D(matmul(array(D0), array(H)), HDiagonal, "D0 H", "diagonal of H", errThresh_in)
+        DH_nxnm1 = matmul(array(D), array(H))
+        H = [[DH_nxnm1[i, j] for j in range(n - 1)] for i in range(n)]
 
-    # Page 5: "Replace H by R(j)HG(j), A by R(j)DA and B by BER(j)"
-    #
-    # Since A and B are integer matrices, nint() is used to suppress roundoff
-    # error.
-    #
-    R = getR(j, n)         # n x n
-    G = getG(H, j, n)      # n-1 x n-1
-    R_nxn = array(R)
-    RHG_nxnm1 = matmul(matmul(R_nxn, array(H)), array(G))
-    H = [[RHG_nxnm1[i, j] for j in range(n - 1)] for i in range(n)]
-    RD_nxn = matmul(R_nxn, array(D))
-    RDA_nxn = matmul(RD_nxn, array(A))
-    A = [[nint(RDA_nxn[i, j]) for j in range(n)] for i in range(n)]
-    E = getE(D)
-    BE_nxn = matmul(array(B), array(E))
-    BER_nxn = matmul(BE_nxn, R_nxn)
-    B = [[nint(BER_nxn[i, j]) for j in range(n)] for i in range(n)]
+        # Page 5: "Select an integer j, [0 <= j < n − 1 in our zero-based indexing]
+        #         such that gamma^j |H[j][j]| >= gamma^i |H[i][i]| for all i,
+        #         [0 <= i < n − 1 in our indexing]
+        j = indexOfWeightedMax(H, gamma_in, n)
+        print("j:", j)
 
-    # Invatiants to check (here I is the identity matrix):
-    # - (D0 H)[i][j] = H[i][j] if i = j, else 0
-    # - G G-transpose = I (n-1 x n-1)
-    # - AB = I should be the n x n zero matrix
-    # - A H0 - H should be the n x n-1 zero matrix
-    # - xBH = x B A H0 = x H0 should be the 1 x n-1 zero vector (two tests)
-    # - det(A) - 1 = det(B) - 1 = 0
-    #
-    I_nm1  = getIdentity(n-1)
-    I_n    = getIdentity(n)
-    xB_1xn = matmul(x_1xn, array(B))
-    zero   = [0 for i in range(n - 1)]
-    H0cumG_nxnm1 = matmul(H0cumG_nxnm1, array(G))
-    testEqual2D(matmul(array(G), transpose(array(G))), I_nm1, "GGt",       "I")
-    testEqual2D(matmul(array(A), array(B)),            I_n,   "AB",        "I")
-    testEqual2D(matmul(array(A), H0cumG_nxnm1),        H,     "A H0 cumG", "H")
-    testEqual1D(matmul(xB_1xn, array(H)),              zero,  "xBH",       "0")
-    testEqual1D(matmul(x_1xn, H0_nxnm1),               zero,  "xH0",       "0")
-    testEqual1D([abs(det(array(A)))],                  [1],   "|A|",       "1")
-    testEqual1D([abs(det(array(B)))],                  [1],   "|B|",       "1")
+        # Page 5: "Replace H by R(j)HG(j), A by R(j)DA and B by BER(j)"
+        #
+        # Since A and B are integer matrices, nint() is used to suppress roundoff
+        # error.
+        #
+        R = getR(j, n)         # n x n
+        G = getG(H, j, n)      # n-1 x n-1
+        R_nxn = array(R)
+        RHG_nxnm1 = matmul(matmul(R_nxn, array(H)), array(G))
+        H = [[RHG_nxnm1[i, j] for j in range(n - 1)] for i in range(n)]
+        RD_nxn = matmul(R_nxn, array(D))
+        RDA_nxn = matmul(RD_nxn, array(A))
+        A = [[nint(RDA_nxn[i, j]) for j in range(n)] for i in range(n)]
+        E = getE(D, n)
+        BE_nxn = matmul(array(B), array(E))
+        BER_nxn = matmul(BE_nxn, R_nxn)
+        B = [[nint(BER_nxn[i, j]) for j in range(n)] for i in range(n)]
 
-    # Increment the number of iterations
-    #    
-    numIterations += 1
+        # Invatiants to check (here I is the identity matrix):
+        # - (D0 H)[i][j] = H[i][j] if i = j, else 0
+        # - G G-transpose = I (n-1 x n-1)
+        # - AB = I should be the n x n zero matrix
+        # - A H0 - H should be the n x n-1 zero matrix
+        # - xBH = x B A H0 = x H0 should be the 1 x n-1 zero vector (two tests)
+        # - det(A) - 1 = det(B) - 1 = 0
+        #
+        I_nm1  = getIdentity(n-1)
+        I_n    = getIdentity(n)
+        xB_1xn = matmul(x_1xn, array(B))
+        zero   = [0 for i in range(n - 1)]
+        H0cumG_nxnm1 = matmul(H0cumG_nxnm1, array(G))
+        testEqual2D(matmul(array(G), transpose(array(G))), I_nm1, "GGt",       "I", errThresh_in)
+        testEqual2D(matmul(array(A), array(B)),            I_n,   "AB",        "I", errThresh_in)
+        testEqual2D(matmul(array(A), H0cumG_nxnm1),        H,     "A H0 cumG", "H", errThresh_in)
+        testEqual1D(matmul(xB_1xn, array(H)),              zero,  "xBH",       "0", errThresh_in)
+        testEqual1D(matmul(x_1xn, H0_nxnm1),               zero,  "xH0",       "0", errThresh_in)
+        testEqual1D([abs(det(array(A)))],                  [1],   "|A|",       "1", errThresh_in)
+        testEqual1D([abs(det(array(B)))],                  [1],   "|B|",       "1", errThresh_in)
+        printFloatMatrix(D0, "D0")
+        printIntMatrix(D, "D")
+        printFloatMatrix(H, "H")
 
-    
+        # Increment the number of iterations
+        #
+        numIterations += 1
+
+# Inputs
+# unnormalizedX = [2, 3, -5]
+unnormalizedX = [1, 2.71828, 3.14159]
+errThresh = 0.00001
+gamma = sqrt(4/3)
+iteratioinAfterWhichToStop = 10
+iterationAfterWhichToDisplay = [2]
+
+# Run the algorithm
+run(unnormalizedX, errThresh, gamma, iteratioinAfterWhichToStop)
